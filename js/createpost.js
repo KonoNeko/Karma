04f30017dee087a615b1a98a541455cbad388ce1
diyarravandi <dravandi@my.bcit.ca>
2021-04-30 20:56:56 -0700

    added more code to createpost.js. Still some bugs that need to be fixed.

--- js/createpost.js
+++ js/createpost.js
@@ -1,12 +1,30 @@
-window.addEventListener('load', function() {
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (this.files && this.files[0]) {
-            var img = document.querySelector('img');
-            img.onload = () => {
-                URL.revokeObjectURL(img.src);  
-            }
+// window.addEventListener('load', function() {
+//     document.querySelector('input[type="file"]').addEventListener('change', function() {
+//         if (this.files && this.files[0]) {
+//             var img = document.querySelector('img');
+//             img.onload = () => {
+//                 URL.revokeObjectURL(img.src);  
+//             }
   
-            img.src = URL.createObjectURL(this.files[0]); 
-        }
-    });
-  });
+//             img.src = URL.createObjectURL(this.files[0]); 
+//         }
+//     });
+//   });
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
