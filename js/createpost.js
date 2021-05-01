a14188958647cb115303897e0070c40dd684f41a
diyarravandi <dravandi@my.bcit.ca>
2021-04-30 20:03:20 -0700

    started createpost.js

---
+++ js/createpost.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('img');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);  
+            }
+  
+            img.src = URL.createObjectURL(this.files[0]); 
+        }
+    });
+  });
