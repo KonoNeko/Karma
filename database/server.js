a939470f2c47732f54e41f63c32dde02037aa386
marlonrenzo <mfajardo3@my.bcit.ca>
2021-05-04 00:06:40 -0700

    Added filtering to profile endpoint (wip, bugs with object parsing)

--- database/server.js
+++ database/server.js
@@ -1,5 +1,6 @@
 const ENDPOINT = "/karma/v1";
 
+const filter = require('./modules/object_parsing');
 const express = require("express");
 const app = express();
 const mysql = require('mysql');
@@ -23,7 +24,7 @@ app.use(express.urlencoded( {extended: true} ));
 app.get(ENDPOINT + '/posts', (req, res) => {
     const sql = "SELECT * FROM posts_feed";
     db.query(sql, (err, result) => {
-        if (err) throw errl
+        if (err) throw err;
         let resultText = JSON.stringify(result);
         res.end(resultText);
     });
@@ -33,9 +34,16 @@ app.get(ENDPOINT + '/profiles/:id', (req, res) => {
     const id = req.params.id;
     let sql = `CALL get_profile_info('${id}');`;
     db.query(sql, (err, result) => {
-        if (err) throw errl
-        let resultText = JSON.stringify(result);
-        res.end(resultText);
+        if (err) throw err;
+        let response = result[0];
+        let profile = {
+            info: filter.profile(response[0]),
+            education: filter.education(response),
+            skills: filter.skills(response),
+            experience: filter.experience(response),
+            certifications: filter.experience(response)
+        };
+        res.end(JSON.stringify(profile));
     });
 });
 
