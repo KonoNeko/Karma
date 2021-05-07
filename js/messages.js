8b0f3d7c9c665ee523057e36ed826560a3058bb9
Christina Raganit <craganit@my.bcit.ca>
2021-05-06 17:28:25 -0700

    messages almsot done 2/3

--- js/messages.js
+++ js/messages.js
@@ -17,6 +17,8 @@ function loadAllMessagers() {
 
 function loadAllMessages() {
   generateMessages();
+  createMessageSentByOtherUser();
+  createMessageSentByOtherUser();
 }
 
 function generateMessager() {
@@ -58,11 +60,78 @@ function generateMessager() {
   mainMessagesDiv.appendChild(messagerDiv);
 }
 
-function generateMessages() {}
+function generateMessages() {
+  let messagerInformationDiv = document.getElementById(
+    "messages-user-information"
+  );
+
+  let messagerImgDiv = document.createElement("div");
+  let messagerImg = document.createElement("img");
+  let messagerName = document.createElement("p");
+  let messagerUsername = document.createElement("p");
+
+  messagerImgDiv.setAttribute("class", "smallMessagerImgDiv profilepicture");
+  messagerImg.src = "./images/placeholder1.png";
+  messagerName.innerHTML = "name of messenger";
+  messagerName.setAttribute("class", "bodytitle");
+  messagerUsername.innerHTML = "@" + "messenger.username";
+  messagerUsername.setAttribute("class", "bodytext");
+
+  messagerImgDiv.appendChild(messagerImg);
+  let messagerAuthorText = document.createElement("div");
+  messagerAuthorText.appendChild(messagerName);
+  messagerAuthorText.appendChild(messagerUsername);
+  messagerAuthorText.setAttribute("class", "messagerText");
+  messagerAuthorText.setAttribute("style", "padding-left: 10px");
+
+  messagerInformationDiv.setAttribute("style", "padding-bottom: 20px");
+
+  messagerInformationDiv.appendChild(messagerImgDiv);
+  messagerInformationDiv.appendChild(messagerAuthorText);
+}
 
 function generateLine() {
   let hr = document.createElement("hr");
 
   let mainMessagesDiv = document.getElementById("messages");
   mainMessagesDiv.appendChild(hr);
+}
+
+function createMessageSentByYou() {}
+
+function createMessageSentByOtherUser() {
+  let messagesExpandedDiv = document.getElementById("messages-expanded");
+
+  let messagesSentDiv = document.createElement("div");
+  messagesSentDiv.setAttribute("class", "messagesSentDiv");
+
+  let messagesSentImgDiv = document.createElement("div");
+  let messagesSentImg = document.createElement("img");
+  messagesSentImg.src = "./images/placeholder2.png";
+  messagesSentImgDiv.setAttribute("class", "messagesSentImgDiv profilepicture");
+  messagesSentImgDiv.appendChild(messagesSentImg);
+
+  let messagesSentTextDiv = document.createElement("div");
+  messagesSentTextDiv.setAttribute("class", "messagesSentTextDiv");
+  let messagesSentAuthor = document.createElement("p");
+  let messagesSentText = document.createElement("div");
+  let messagesSentTime = document.createElement("p");
+
+  messagesSentAuthor.setAttribute("class", "bodytitle");
+  messagesSentText.setAttribute("class", "bodytext messagesSentText");
+  messagesSentTime.setAttribute("class", "smalltext");
+
+  messagesSentTextDiv.appendChild(messagesSentAuthor);
+  messagesSentTextDiv.appendChild(messagesSentText);
+  messagesSentTextDiv.appendChild(messagesSentTime);
+
+  messagesSentAuthor.innerHTML = "williamblack";
+  messagesSentText.innerHTML =
+    "what is UP homeboys and homegirls and hometheys today we are going to be volunteering at Burnaby Park Secondary School. We're gonna hit up the gym to referee for their first boys's soccer game of the season. Stay tuned and make sure to like and subscribe for more content!";
+  messagesSentTime.innerHTML = "7 minutes " + "ago";
+
+  messagesSentDiv.appendChild(messagesSentImgDiv);
+  messagesSentDiv.appendChild(messagesSentTextDiv);
+
+  messagesExpandedDiv.appendChild(messagesSentDiv);
 }
