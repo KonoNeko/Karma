465ba003d43d100174c9899a32c2df502fc2ad64
marlonrenzo <marlonrfajardo@gmail.com>
2021-05-08 02:04:21 -0700

    Added categories

---
+++ database/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO volunteer_categories (category)
+VALUES
+("Recommended For You"),
+("Fine Arts and Culture"),
+("Sports and Recreation"),
+("Literacy, Libraries, and Learning"),
+("Environment"),
+("Health and Wellness"),
+("Computers and Information Technology");
