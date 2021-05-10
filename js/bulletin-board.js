171e0dac2f91be341b2cf0d5c350f3a6678927c1
dhc20067227@gmail.com <dhc20067227@gmail.com>
2021-05-10 15:58:37 -0700

    create the bulletin board for organization. add the button to the page that will create the volunteer post

--- js/bulletin-board.js
+++ js/bulletin-board.js
@@ -118,7 +118,6 @@ function generateOpportunity(category) {
     span.onclick = function () {
       modal.style.display = "none";
     };
-
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
       if (event.target == modal) {
