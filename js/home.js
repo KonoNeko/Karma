1e4b6fa1ff9363916a6b5db56891808e3c4a91ae
Christina Raganit <craganit@my.bcit.ca>
2021-05-10 21:41:40 -0700

    landing page start + home page madness

--- js/home.js
+++ js/home.js
@@ -28,12 +28,10 @@ function loadStories() {
   createStory(story);
 }
 
-
 function loadPosts() {
   let post = document.getElementById("posts");
   createPost(post);
 }
-
 
 function loadWhatsNew() {
   createWhatsNew();
@@ -43,78 +41,86 @@ function loadRecommendedConnections() {
   createRecommendedConnection();
 }
 
-
 // CREATE INDIVIDUAL UI ELEMENTS
 function createStory(stories) {
-
   let storyImgDiv = document.createElement("div");
   storyImgDiv.setAttribute("class", "profilepic");
   storyImgDiv.setAttribute("style", "padding-bottom: 10px");
 
-  storyImgDiv.setAttribute("style", "background-image: url('./images/placeholder.jpg')");
+  storyImgDiv.setAttribute(
+    "style",
+    "background-image: url('./images/placeholder.jpg')"
+  );
 
   let userName = document.createElement("p");
-  userName.setAttribute("class", "userName")
+  userName.setAttribute("class", "userName");
   userName.innerHTML = "User name";
 
   let storyDiv = document.createElement("div");
   storyDiv.setAttribute("class", "story");
   storyDiv.appendChild(storyImgDiv);
   storyDiv.appendChild(userName);
-  
-  stories.appendChild(storyDiv);
 
+  stories.appendChild(storyDiv);
 }
 
 function createPost(post) {
-
   let topPartDiv = document.createElement("div");
+  topPartDiv.setAttribute("class", "topPartDiv");
 
-  let postImgDiv = document.createElement("div")
-  postImgDiv.setAttribute("class", "profilepic");
+  let postImgDiv = document.createElement("div");
+  postImgDiv.setAttribute("class", "profilepic topPartDivColumn topPartDivImg");
   postImgDiv.setAttribute("style", "padding-bottom: 10px");
 
-  postImgDiv.setAttribute("style", "background-image: url('./images/placeholder.jpg')");
+  postImgDiv.setAttribute(
+    "style",
+    "background-image: url('./images/placeholder.jpg')"
+  );
+
+  let nameAndTimeDiv = document.createElement("div");
+  nameAndTimeDiv.setAttribute("class", "name-and-time topPartDivColumn");
 
   let userName = document.createElement("p");
-  userName.setAttribute("class", "userName")
+  userName.setAttribute("class", "userName");
   userName.innerHTML = "User name";
 
   let timePosted = document.createElement("p");
   timePosted.setAttribute("class", "timePosted");
   timePosted.innerHTML = "30 minutes ago";
 
+  nameAndTimeDiv.appendChild(userName);
+  nameAndTimeDiv.appendChild(timePosted);
+
   topPartDiv.appendChild(postImgDiv);
-  topPartDiv.appendChild(userName);
-  topPartDiv.appendChild(timePosted);
+  topPartDiv.appendChild(nameAndTimeDiv);
 
   post.appendChild(topPartDiv);
 
-
   let picture = document.createElement("div");
-  picture.setAttribute("class", "postpicture")
-  picture.setAttribute("style", "background-image: url('./images/placeholder.jpg')");
+  picture.setAttribute("class", "postpicture");
+  picture.setAttribute(
+    "style",
+    "background-image: url('./images/placeholder.jpg')"
+  );
   post.appendChild(picture);
-
 
   let captionAndComments = document.createElement("div");
   captionAndComments.setAttribute("class", "captionAndComments");
   let likes = document.createElement("p");
   let caption = document.createElement("p");
   let comments = document.createElement("p");
-  likes.setAttribute("class", "likes")
-  caption.setAttribute("class", "caption")
-  comments.setAttribute("class", "comments")
+  likes.setAttribute("class", "likes");
+  caption.setAttribute("class", "caption");
+  comments.setAttribute("class", "comments");
 
   likes.innerHTML = "williamblack and 103 others like this post";
-  caption.innerHTML = "john.doe “The best way to find yourself is to lose yourself in the service of others.” - Mahatma Gandhi";
+  caption.innerHTML =
+    "john.doe “The best way to find yourself is to lose yourself in the service of others.” - Mahatma Gandhi";
   comments.innerHTML = "View all 54 comments";
 
   post.appendChild(likes);
   post.appendChild(caption);
   post.appendChild(comments);
-  
-
 
   // let postImgDiv = document.createElement("div")
   // postImgDiv.setAttribute("class", "profilepic");
@@ -138,7 +144,6 @@ function createPost(post) {
   // userNameAndCaption.setAttribute("class", "userNameAndCaption");
   // userNameAndCaption.innerHTML = "User name - asdakdjkaljdlkajdklajdlkajdakl";
 
-
   // let postsDiv = document.createElement("div");
   // postsDiv.setAttribute("class", "posts");
   // postsDiv.appendChild(postImgDiv);
@@ -146,13 +151,10 @@ function createPost(post) {
   // postsDiv.appendChild(timePosted);
   // postsDiv.appendChild(likes);
   // postsDiv.appendChild(userNameAndCaption);
-  
-  // post.appendChild(postsDiv);
 
+  // post.appendChild(postsDiv);
 }
 
 function createWhatsNew() {}
 
 function createRecommendedConnection() {}
-
-
