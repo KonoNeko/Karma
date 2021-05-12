639374338573c2ccaca73015fd47d579cb82575f
Christina Raganit <craganit@my.bcit.ca>
2021-05-11 21:33:26 -0700

    flexbox magic

--- js/home.js
+++ js/home.js
@@ -44,9 +44,8 @@ function loadWhatsNew() {
 
 function loadRecommendedConnections() {
   let recommendedUsers = document.getElementById("recommended-connections");
-  
+
   let heading2 = document.createElement("hr");
-  heading2.setAttribute("class", "headers");
   recommendedUsers.appendChild(heading2);
 
   createRecommendedConnection(recommendedUsers);
@@ -55,9 +54,9 @@ function loadRecommendedConnections() {
 
   let viewAll = document.createElement("a");
   viewAll.id = "view-all";
-  viewAll.href = "";
   viewAll.innerHTML = "View all recommended connections";
-  viewAll.style = "margin-top: auto";
+  viewAll.setAttribute("style", "margin-top: 20px");
+  // viewAll.setAttribute("href", ""); // YOU NEED SOMETHING IN HERE OR IT BREAKS THE CODE
 
   recommendedUsers.appendChild(viewAll);
 }
@@ -154,9 +153,8 @@ function createRecommendedConnection(recommended) {
   recommendedUserDiv.setAttribute("class", "recommendedUserDiv");
 
   let storyImgDiv = document.createElement("div");
-  storyImgDiv.setAttribute("class", "profilepic");
-  storyImgDiv.setAttribute("style", "padding-bottom: 10px");
-
+  storyImgDiv.setAttribute("class", "profilepic storyImgDiv");
+  storyImgDiv.setAttribute("style", "padding-bottom: 10px; width: 20%");
   storyImgDiv.setAttribute(
     "style",
     "background-image: url('./images/placeholder.jpg')"
@@ -164,6 +162,7 @@ function createRecommendedConnection(recommended) {
 
   let nameAndUserName = document.createElement("div");
   nameAndUserName.setAttribute("class", "name-and-userName");
+  nameAndUserName.setAttribute("style", "width: 50%;");
 
   let userName = document.createElement("p");
   userName.setAttribute("class", "userNames");
@@ -180,17 +179,27 @@ function createRecommendedConnection(recommended) {
   recommendedUserDiv.appendChild(nameAndUserName);
 
   let followUser = document.createElement("div");
-  followUser.innerHTML = "FOLLOW";
+  let followUserButton = document.createElement("button");
+
+  followUserButton.innerHTML = "FOLLOW";
+  followUserButton.setAttribute("class", "followUserButton");
   followUser.setAttribute("class", "followUser");
+  followUser.setAttribute("style", "width: 40%");
+  followUserButton.setAttribute("style", "width: 70%; min-width: 100px;");
+
+  followUser.appendChild(followUserButton);
 
   recommendedUserDiv.appendChild(followUser);
 
   let heading2 = document.createElement("hr");
-  heading2.setAttribute("class", "headers");
+  heading2.setAttribute("style", "margin-top: 20px; margin-bottom: 20px;");
 
   recommended.appendChild(recommendedUserDiv);
+  recommendedUserDiv.setAttribute(
+    "style",
+    "margin-top: 10px; margin-bottom: 10px;"
+  );
   recommended.appendChild(heading2);
-  
 
   // let recommendedConnections = document.createElement("p");
   // recommendedConnections.setAttribute("class", "recommendedConnections");
@@ -200,7 +209,6 @@ function createRecommendedConnection(recommended) {
 }
 
 function createWhatsNew(newPost) {
-
   let opportunityRole = document.createElement("p");
   opportunityRole.innerHTML = "opportunityRole Role Role";
   opportunityRole.setAttribute("class", "heading3");
