a0cbe626599ba50266dd4091dcf604a7cef1d59c
marlonrenzo <mfajardo3@my.bcit.ca>
2021-05-14 20:45:15 -0700

    Added functionality to hide and display modal

--- public/js/commentsection.js
+++ public/js/commentsection.js
@@ -5,27 +5,117 @@ function loadHome() {
 
 function loadPost() {
     const placeholderImg = './images/placeholder.jpg';
-    createModal();
+    createModal(1);
     displayPost(placeholderImg);
+    displayPostDetails("Username", "30 minutes ago", placeholderImg);
     createComments(placeholderImg);
 }
 
-function createModal() {
+function createModal(postID) {
+    let overlay = document.createElement("div");
+    overlay.id = "modalOverlay";
+    overlay.onclick = hideModal;
+
     let modal = document.createElement("div");
-    modal.id = "postModal1";
+    modal.className = "postModal";
+    modal.id = postID;
 
     let leftSideDiv = document.createElement("div");
     leftSideDiv.id = "postPicture";
     leftSideDiv.className = "leftDiv";
 
     let rightSideDiv = document.createElement("div");
-    leftSideDiv.id = "rightDiv";
-    leftSideDiv.className = "rightDiv";
+    rightSideDiv.id = "rightDiv";
+    rightSideDiv.className = "rightDiv";
+
+        let postDetails = document.createElement("div");
+        postDetails.id = "postOwnerTitle";
+
+            let profilePic = document.createElement("div");
+            profilePic.id = "postOwnerProfilePic";
+            profilePic.className = "profilepic";
+
+            let nameAndTime = document.createElement("div");
+            nameAndTime.id = "nameAndTimeDiv";
+
+                let username = document.createElement("p");
+                username.id = "postOwnerUsername";
+
+                let time = document.createElement("p");
+                time.id = "timePosted";
+            
+            nameAndTime.appendChild(username);
+            nameAndTime.appendChild(time);
+        
+        postDetails.appendChild(profilePic);
+        postDetails.appendChild(nameAndTime);
+        rightSideDiv.appendChild(postDetails);
+
+ 
+        rightSideDiv.appendChild(createLine());
+
+        let commentList = document.createElement("div");
+        commentList.id = "commentList";
+
+        rightSideDiv.appendChild(commentList);
+        rightSideDiv.appendChild(createLine());
+
+        let interactionDiv = document.createElement("div");
+        interactionDiv.id = "interactionDiv";
+
+            let interactionButtons = document.createElement("div");
+            interactionButtons.id = "interactionButtons";
+
+            let likesLine = document.createElement("p");
+            likesLine.innerHTML = `<span id="likeUsername"></span> and <span id="likes"></span> others like this post`;
+
+        interactionDiv.appendChild(interactionButtons);
+        interactionDiv.appendChild(likesLine);
+        rightSideDiv.appendChild(interactionDiv);
 
     modal.appendChild(leftSideDiv);
     modal.appendChild(rightSideDiv);
+    overlay.appendChild(modal);
 
-    document.body.appendChild(modal);
+    document.body.appendChild(overlay);
+}
+
+function hideModal() {
+    document.getElementById("modalOverlay").style.display = "none";
+}
+
+function displayModal() {
+    document.getElementById("modalOverlay").style.display = "block";
+}
+
+
+function displayPost(placeholderImg) {
+    let postImage = document.getElementById("postPicture");
+    postImage.setAttribute("style", `background-image: url('${placeholderImg}')`);
+}
+
+function displayPostDetails(username, time, placeholderImg) {
+    let postOwnerTitle = document.getElementById("postOwnerTitle");
+
+    let posterProfilePic = document.getElementById("postOwnerProfilePic");
+    posterProfilePic.setAttribute("style", `background-image: url('${placeholderImg}')`);
+
+    let nameAndTimeDiv = document.getElementById("nameAndTimeDiv");
+    let userName = document.getElementById("postOwnerUsername");
+    userName.innerHTML = username;
+    let timePosted = document.getElementById("timePosted");
+    timePosted.innerHTML = time;
+    nameAndTimeDiv.appendChild(userName);
+    nameAndTimeDiv.appendChild(timePosted);
+
+    postOwnerTitle.appendChild(posterProfilePic);
+    postOwnerTitle.appendChild(nameAndTimeDiv);
+}
+
+
+function createLine() {
+    let line = document.createElement("hr");
+    return line;
 }
 
 function createComment(profilePic, username, comment, timestamp, commentID) {
@@ -62,6 +152,7 @@ function createComment(profilePic, username, comment, timestamp, commentID) {
     return commentDiv;
 }
 
+
 function createCommentParagraph(username, comment) {
     let commentParagraph = document.createElement("p");
     let commentUserName = document.createElement("span");
@@ -82,6 +173,7 @@ function createCommentParagraph(username, comment) {
     return commentParagraph;
 }
 
+
 function loadCaption(profilePic, username, caption) {
     let captionDiv = document.createElement("div");
     captionDiv.className = "comment";
@@ -99,30 +191,8 @@ function loadCaption(profilePic, username, caption) {
     return captionDiv;
 }
 
-function displayPost(placeholderImg) {
-    let postImage = document.getElementById("postPicture");
-    postImage.setAttribute("style", `background-image: url('${placeholderImg}')`);
-}
-
 
 function createComments(placeholderImg) {
-
-    let postOwnerTitle = document.getElementById("postOwnerTitle");
-    let posterProfilePic = document.getElementById("postOwnerProfilePic");
-    posterProfilePic.setAttribute("style", `background-image: url('${placeholderImg}')`);
-
-
-    let nameAndTimeDiv = document.getElementById("nameAndTimeDiv");
-    let userName = document.getElementById("postOwnerUsername");
-    userName.innerHTML = "User name";
-    let timePosted = document.getElementById("timePosted");
-    timePosted.innerHTML = "30 minutes ago";
-    nameAndTimeDiv.appendChild(userName);
-    nameAndTimeDiv.appendChild(timePosted);
-
-    postOwnerTitle.appendChild(posterProfilePic);
-    postOwnerTitle.appendChild(nameAndTimeDiv);
-
     let commentsDiv = document.getElementById("commentList");
 
 
