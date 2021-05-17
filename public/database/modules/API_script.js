8f47444b2476da7d7875172c988e4bb0545741b2
marlonrenzo <marlonrfajardo@gmail.com>
2021-05-17 16:02:59 -0700

    Added code for editing profile (WIP)

--- public/database/modules/API_script.js
+++ public/database/modules/API_script.js
@@ -1,6 +1,39 @@
-const ENDPOINT = "https://marlonfajardo.ca/karma/v1";
+const BASE_URL = "https://marlonfajardo.ca/karma/v1";
 
-const endpoint_methods = {
-    1: ["GET", "/posts"],
-    2: ["GET", "/posts/userID"] 
-  };
+
+
+function editProfileSection(username, section, newData, method) {
+
+  const profileSections = {
+    "/education": {
+      "ADD": "",
+      "EDIT": "",
+      "REMOVE": ""
+    },
+    "/experience": {
+      "ADD": "",
+      "EDIT": "",
+      "REMOVE": ""
+    },
+    "/skills": {
+      "ADD": "",
+      "REMOVE": ""
+    },
+    "/awardsAndCertification": {
+      "ADD": "",
+      "EDIT": "",
+      "REMOVE": ""
+    },
+
+  }
+  console.log(method + ": " + BASE_URL + endpoint);
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open(method, url + endpoint, true);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("result").innerText = this.responseText;
+      }
+  }
+}
