17f5ce088a38f5fa81e0a3c0960890e588b66d01
marlonrenzo <marlonrfajardo@gmail.com>
2021-05-18 22:43:00 -0700

    Small changes to add bulletin board dynamically (WIP)

--- public/js/bulletin-board.js
+++ public/js/bulletin-board.js
@@ -1,14 +1,182 @@
-function loadOpportunities() {
-  loadRecommendedOpportunities();
-  loadFineArtsOpportunities();
-  loadSportsOpportunities();
-  loadLiteracyOpportunities();
-  loadEnvironmentOpportunities();
-  loadHealthOpportunities();
-  loadComputersOpportunities();
+
+const BASE_URL = "https://marlonfajardo.ca/karma/v1";
+
+const result = {
+  "Fine Arts and Culture": {},
+  "Sports and Recreation": {
+  "7": {
+  "opportunity_id": 7,
+  "employer": "Bonsor Community Center",
+  "category": "Sports and Recreation",
+  "event_date": "2021-06-22T04:00:00.000Z",
+  "poster_id": 2,
+  "title": "Camp Assistant",
+  "description": "Assist Camp leaders with daily activities",
+  "requirements": "No requirements",
+  "image_url": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png",
+  "post_date": "2021-05-13T20:39:10.000Z",
+  "posted_by": "Karma"
+  },
+  "8": {
+  "opportunity_id": 8,
+  "employer": "Edmonds Community Center",
+  "category": "Sports and Recreation",
+  "event_date": "2021-05-25T04:00:00.000Z",
+  "poster_id": 2,
+  "title": "Concession Attendant",
+  "description": "Sell drinks and snacks to people",
+  "requirements": "No requirements",
+  "image_url": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png",
+  "post_date": "2021-05-13T20:39:10.000Z",
+  "posted_by": "Karma"
+  }
+  },
+  "Literacy, Libraries, and Learning": {
+  "1": {
+  "opportunity_id": 1,
+  "employer": "Guildford Library",
+  "category": "Literacy, Libraries, and Learning",
+  "event_date": "2021-05-30T04:00:00.000Z",
+  "poster_id": 2,
+  "title": "Library Assistant",
+  "description": "Be an assistant at a Library",
+  "requirements": "1-2 years experience at a library",
+  "image_url": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png",
+  "post_date": "2021-05-10T21:46:25.000Z",
+  "posted_by": "Karma"
+  }
+  },
+  "Environment": {
+  "5": {
+  "opportunity_id": 5,
+  "employer": "Team Karma",
+  "category": "Environment",
+  "event_date": "2021-05-30T04:00:00.000Z",
+  "poster_id": 2,
+  "title": "Clean Up Crew Member",
+  "description": "Clean up the Fraser river. All clothing and equipment provided",
+  "requirements": "No requirements",
+  "image_url": "https://coconuts.co/wp-content/uploads/2018/03/dirty-river.jpg",
+  "post_date": "2021-05-10T22:35:35.000Z",
+  "posted_by": "Karma"
+  },
+  "9": {
+  "opportunity_id": 9,
+  "employer": "Riverway Sports Complex",
+  "category": "Environment",
+  "event_date": "2021-06-30T04:00:00.000Z",
+  "poster_id": 2,
+  "title": "Park Clean Up",
+  "description": "Pick up trash and make sure park is clean",
+  "requirements": "No requirements",
+  "image_url": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png",
+  "post_date": "2021-05-13T20:39:10.000Z",
+  "posted_by": "Karma"
+  }
+  },
+  "Health and Wellness": {
+  "10": {
+  "opportunity_id": 10,
+  "employer": "Downtown Vancouver",
+  "category": "Health and Wellness",
+  "event_date": "2021-05-12T04:00:00.000Z",
+  "poster_id": 2,
+  "title": "Feeding the Homeless",
+  "description": "Hand out food to the homeless",
+  "requirements": "No requirements",
+  "image_url": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png",
+  "post_date": "2021-05-13T20:39:10.000Z",
+  "posted_by": "Karma"
+  },
+  "12": {
+  "opportunity_id": 12,
+  "employer": "Team Karma",
+  "category": "Health and Wellness",
+  "event_date": "2021-05-21T04:00:00.000Z",
+  "poster_id": 2,
+  "title": "Helping out at Senior Home",
+  "description": "Hang out with the Seniors",
+  "requirements": "No requirements",
+  "image_url": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png",
+  "post_date": "2021-05-13T20:56:44.000Z",
+  "posted_by": "Karma"
+  }
+  },
+  "Computers and Information Technology": {}
+};
+
+const categories = {
+  "Recommended For You": "recommended-category",
+  "Fine Arts and Culture": "fine-arts-category",
+  "Computers and Information Technology": "computers-category",
+  "Environment": "environment-category",
+  "Health and Wellness": "health-category",
+  "Literacy, Libraries, and Learning": "literacy-category",
+  "Sports and Recreation": "sports-category"
+};
+
+function formatTimestamp(timestamp) {
+  let dateObj = new Date(Date.parse(timestamp));
+  return returnHighestTimeDiff(dateObj);
 }
 
-function loadRecommendedOpportunities() {
+function returnHighestTimeDiff(time) {
+  let ms = {
+    y: 31536000000,
+    w: 604800000,
+    d: 86400000,
+    h: 3600000,
+    m: 60000,
+  }
+  let diff = Date.now() - time;
+  for(let [key, value] of Object.entries(ms)) {
+      if (diff > value) {
+          return `${Math.floor(diff/value)}${key}`;
+      } else if (diff < ms.m) {
+        return "Just now";
+      }
+  }
+}
+
+function view_social_feed(userID) {
+  const method = "GET";
+  const endpoint = "/posts";
+  const params = `/${userID}`;
+  const url = BASE_URL + endpoint + params;
+  APIRequest(method, url); 
+}
+
+
+function APIRequest(method, url) {
+  console.log(method + ": " + url);
+  const xhttp = new XMLHttpRequest();
+  xhttp.open(method, url, true);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          // let result = JSON.parse(this.responseText);
+          // console.log("loading post");
+          // console.log(result);
+          for (let i=0; i<result.length; i++) {
+              createPost(result[i]);
+          }
+          
+      }
+  }
+}
+
+function loadOpportunities() {
+  
+  // loadRecommendedOpportunities();
+  // loadFineArtsOpportunities();
+  // loadSportsOpportunities();
+  // loadLiteracyOpportunities();
+  // loadEnvironmentOpportunities();
+  // loadHealthOpportunities();
+  // loadComputersOpportunities();
+}
+
+function loadRecommendedOpportunities(oppObj) {
   let category = document.getElementById("recommended-opportunities");
   generateOpportunity(category);
   generateOpportunity(category);
@@ -18,7 +186,7 @@ function loadRecommendedOpportunities() {
   generateOpportunity(category);
 }
 
-function loadFineArtsOpportunities() {
+function loadFineArtsOpportunities(oppObj) {
   let category = document.getElementById("fine-arts-opportunities");
   generateOpportunity(category);
   generateOpportunity(category);
@@ -28,7 +196,7 @@ function loadFineArtsOpportunities() {
   generateOpportunity(category);
 }
 
-function loadSportsOpportunities() {
+function loadSportsOpportunities(oppObj) {
   let category = document.getElementById("sports-opportunities");
   generateOpportunity(category);
   generateOpportunity(category);
@@ -38,7 +206,7 @@ function loadSportsOpportunities() {
   generateOpportunity(category);
 }
 
-function loadLiteracyOpportunities() {
+function loadLiteracyOpportunities(oppObj) {
   let category = document.getElementById("literacy-opportunities");
   generateOpportunity(category);
   generateOpportunity(category);
@@ -48,37 +216,32 @@ function loadLiteracyOpportunities() {
   generateOpportunity(category);
 }
 
-function loadEnvironmentOpportunities() {
+function loadEnvironmentOpportunities(oppObj) {
   let category = document.getElementById("environment-opportunities");
   generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
+
 }
 
-function loadHealthOpportunities() {
+function loadHealthOpportunities(oppObj) {
   let category = document.getElementById("health-opportunities");
   generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
+
 }
 
-function loadComputersOpportunities() {
+function loadComputersOpportunities(oppObj) {
   let category = document.getElementById("computers-opportunities");
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
-  generateOpportunity(category);
+  category.style.display = block;
+  generateOpportunity(postObj, category);
+
 }
 
-function generateOpportunity(category) {
+function loadOpportunity(oppObj) {
+  let categoryID = categories[postObj.category];
+  let category = document.getElementById(categoryID);
+  generateOpportunity(oppObj, category)
+}
+
+function generateOpportunity(oppObj, category) {
   let opportunityRole = document.createElement("p");
   opportunityRole.innerHTML = "opportunityRole Role Role";
   opportunityRole.setAttribute("class", "heading3");
