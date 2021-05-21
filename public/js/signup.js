4c15c895720e365e5a11ebcb17c97775313003aa
marlonrenzo <marlonrfajardo@gmail.com>
2021-05-21 14:00:22 -0700

    Fixed sign up process including unique usernames (check firebase console)

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,134 +1,114 @@
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-   var firebaseConfig = {
-    apiKey: "AIzaSyA5oEJUCOc3V4zgGI9-wwMWmd-P6opmnWI",
-    authDomain: "karma-535f3.firebaseapp.com",
-    databaseURL: "https://karma-535f3-default-rtdb.firebaseio.com",
-    projectId: "karma-535f3",
-    storageBucket: "karma-535f3.appspot.com",
-    messagingSenderId: "1023587584355",
-    appId: "1:1023587584355:web:89bb521723bf4afd58eb56",
-    measurementId: "G-VTZ4TEWFBW"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+  apiKey: "AIzaSyA5oEJUCOc3V4zgGI9-wwMWmd-P6opmnWI",
+  authDomain: "karma-535f3.firebaseapp.com",
+  databaseURL: "https://karma-535f3-default-rtdb.firebaseio.com",
+  projectId: "karma-535f3",
+  storageBucket: "karma-535f3.appspot.com",
+  messagingSenderId: "1023587584355",
+  appId: "1:1023587584355:web:89bb521723bf4afd58eb56",
+  measurementId: "G-VTZ4TEWFBW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
   
-  const db = firebase.firestore();
-
- 
+const db = firebase.firestore();
 
 
-// const txtEmail = document.getElementById("email");
-// const textPassword = document.getElementById("password");
-// const login = document.getElementById("btn-login");
-// const signup = document.getElementById("btn-signup");
-// const logout = document.getElementById("btn-logout");
+$("#btn-login").click(function(){
+  var email = $("#email").val();
+  var password = $("#password").val();
 
-// login.addEventListener("click", e => {
-//   const email = txtEmail.value;
-//   const pass = textPassword.value;
-//   const auth = firebase.auth();
+  if (email != "" && password != "")
+  {
+    var result = firebase.auth().signInWithEmailAndPassword(email, password);
 
-//   //Sign in
+    result.catch(function(error){
 
-//   const promise = auth.signInWithEmailAndPassword(email, pass);
-//   promise.catch (e => console.log (e.message));
+      var errorCode = error.code;
+      var errorMessage = error.message;
 
-// });
+      console.log(errorCode);
+      window.alert("Message: " + errorMessage);
+    });
 
-// signup.addEventListener("click", e => {
-//   const email = txtEmail.value;
-//   const pass = textPassword.value;
-//   const auth = firebase.auth();
+  }
+  else
+  {
+    window.alert("Please fill out all fields.");    }
 
-//   //Sign in
+});
 
-//   const promise = auth.createUserWithEmailAndPassword(email, pass);
-//   promise.catch (e => console.log (e.message));
-
-// });
-
-
-// logout.addEventListener("click", e => {
-//   firebase.auth().signOut();
-// })
-
-
-
-
-
-
-// firebase.auth().onAuthStateChanged(firebaseUser => {
-// if(firebaseUser) {
-//   console.log(firebaseUser);
-// }
-// });
-
-
-
-  $("#btn-login").click(function(){
-    var email = $("#email").val();
-    var password = $("#password").val();
-
-    if (email != "" && password != "")
-    {
-      var result = firebase.auth().signInWithEmailAndPassword(email, password);
-
-      result.catch(function(error){
-
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        console.log(errorCode);
-        window.alert("Message: " + errorMessage);
-      });
-
-    }
-    else
-    {
-      window.alert("Please fill out all fields.");    }
-
-  });
-
-
+async function usernameExists() {
+  const citiesRef = db.collection('users');
+  const snapshot = await citiesRef.where('username', '==', $("#username").val()).get();
+  if (snapshot.empty) {
+    return false;
+  } else {
+    return true
+  }
+}
 
   
-  $("#btn-signup").click(function(){
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var cpassword = $("#confirmPassowrd").val();
-    const btn = $("#btn-signup").val();
-
-
-    if (email != "" && password != "" && cpassword != "")
-    {
-      var result = firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
-          db.collection('user').doc(cred.user.uid).set({
-          name: $("#name").val(),
-        });
-      });
-
-      result.catch(function(error){
-
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        console.log(errorCode);
-        window.alert("Message: " + errorMessage);
+$("#btn-signup").click(function(){
+  let email = $("#email").val();
+  let password = $("#password").val();
+  let cpassword = $("#confirmPassowrd").val();
+  let full_name = $("#name").val();
+  let username = $("#username").val();
+  const btn = $("#btn-signup").val();
 
   
-      });
-      
-      
-    }
-    else
-    {
-      window.alert("Please fill out all fields.");    }
 
-  });
+
+  if (email != "" && password != "" && cpassword != "" && full_name != "" && username != "") {
+    usernameExists().then( // ensures usernames are unique
+      (userExists) => {
+        if (userExists) {
+          alert("Unfortunately, that username is taken. Please choose another username.");
+        } else {
+          var result = firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
+            db.collection('users').doc(cred.user.uid).set({
+              email: email,
+              fullName: full_name,
+              username: username 
+            });
+          });
+    
+          result.catch(function(error){
+    
+            var errorCode = error.code;
+            var errorMessage = error.message;
+    
+            console.log(errorCode);
+            window.alert("Message: " + errorMessage);
+    
+
+          });
+        }
+      },
+      (error) => {console.log(error);}
+    );
+    setInterval(() => {
+      usernameExists().then(
+        (userExists) => {
+          firebase.auth().onAuthStateChanged(function (user) {
+            if(user && userExists) { 
+              window.location.href = "home.html"; 
+            }
+          });
+        },
+        (error) => {console.log(error);}
+      );
+    }, 1000);
+  } else {
+    window.alert("Please fill out all fields.");    
+  }
+});
 
   
 
