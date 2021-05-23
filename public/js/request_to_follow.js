8b731fcd4d4da1127474c2572956287e8da1bdbc
marlonrenzo <marlonrfajardo@gmail.com>
2021-05-23 13:13:44 -0700

    Changes to follow request for modularity

--- public/js/request_to_follow.js
+++ public/js/request_to_follow.js
@@ -2,37 +2,36 @@ const BASE_URL = "https://marlonfajardo.ca/karma/v1";
 
 
 
-function APIRequest(method, url) {
+function APIRequest(method, url, callback) {
     console.log(method + ": " + url);
     const xhttp = new XMLHttpRequest();
     xhttp.open(method, url, true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-        // let result = JSON.parse(this.responseText);
-        console.log("success editing profile");
-        // console.log(result);
-        // for (let i=0; i<result.length; i++) {
-        //          (result[i]);
-        // }
+        let response = this.responseText;
+        try {
+          response = JSON.parse(response);
+        } finally {
+          callback(response);
+        }
       }
     };
   }
 
 
 
-  function request_follow(userID, follower) {
-    const method = "POST";
-    const endpoint = "/profiles/followers";
-    const params = formatParams({
-      "id": userID,
-      "follower": follower,
-  
-    });
-    const url = BASE_URL + endpoint + params;
-  
-    return APIRequest(method, url);
-  }
+function request_follow(userID, follower) {
+  const method = "POST";
+  const endpoint = "/profiles/followers";
+  const params = formatParams({
+    "id": userID,
+    "follower": follower,
+  });
+  const url = BASE_URL + endpoint + params;
+
+  APIRequest(method, url, console.log);
+}
   
 
 
