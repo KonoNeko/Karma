89f1cd8163ae9e2511443ee79503d253eae69763
marlonrenzo <marlonrfajardo@gmail.com>
2021-05-24 00:00:48 -0700

    Added procedures for stories

--- public/database/procedures/base_procedures.sql
+++ public/database/procedures/base_procedures.sql
@@ -389,6 +389,48 @@ DELIMITER ;
 
 
 /*
+Posts a new story.
+*/
+DROP PROCEDURE IF EXISTS post_story;
+DELIMITER //
+CREATE PROCEDURE post_story(IN current_username CHAR(50), IN new_image TEXT)
+BEGIN
+    INSERT INTO stories (`profile_id`, `image_url`)
+    VALUES (get_user_id(current_username), new_image);
+END//
+DELIMITER ;
+
+
+/*
+Deletes an existing story.
+*/
+DROP PROCEDURE IF EXISTS delete_story;
+DELIMITER //
+CREATE PROCEDURE post_story(IN current_username CHAR(50), IN current_story INTEGER)
+BEGIN
+    DELETE FROM stories 
+    WHERE profile_id = get_user_id(current_username)
+    AND story_id = current_story
+END//
+DELIMITER ;
+
+
+/*
+Gets all stories from users that the current user is following.
+*/
+DROP PROCEDURE IF EXISTS delete_story;
+DELIMITER //
+CREATE PROCEDURE post_story(IN current_username CHAR(50))
+BEGIN
+    SELECT * FROM stories s
+    WHERE is_following(current_username, s.profile_id)
+    GROUP BY s.profile_id
+    ORDER BY s.timestamp
+END//
+DELIMITER ;
+
+
+/*
 Returns the full name of the opposite user of a user's conversation.
 */
 DROP FUNCTION IF EXISTS get_other_user;
