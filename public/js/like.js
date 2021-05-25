dff04a1e7e80624952ebbe0d204bdd4583d2ad34
Christina <chrisatcst@gmail.com>
2021-05-25 13:28:26 -0700

    just trying to like a post ;-;

--- public/js/like.js
+++ public/js/like.js
@@ -1,30 +1,58 @@
-const URL2 = " https://marlonfajardo.ca/karma/v1/posts"
-const method2 = "POST"
-function like(){
-    let heart = document.getElementById("likeBtn");
-    heart.setAttribute(  "style",
-        "font-size: 24px; color: red; margin-top: 10px; margin-bottom: 10px; margin-right:10px; ");
+const URL2 = " https://marlonfajardo.ca/karma/v1/posts";
+
+const method2 = "POST";
+
+data = getPost();
+
+function like(id) {
+  let heart = document.getElementById(id);
+  let postId = id.slice(-1);
+
+  console.log(postId);
+
+  // LIKE
+  if (heart.className == "far fa-heart likeBtn") {
+    heart.setAttribute(
+      "style",
+      "font-size: 24px; color: #B05A5F; margin-top: 10px; margin-bottom: 10px; margin-right:10px; "
+    );
+    heart.setAttribute("class", "fas fa-heart likeBtn");
+    getPost().then((posts) => {
+      console.log(posts);
+      for (let i = 0; i < posts.length; i++) {
+        if (posts[i].post_info.post_id == postId) {
+          posts[i].post_info.likes + 1;
+        }
+      }
+    });
+  }
+
+  // UNLIKE
+  else {
+    heart.setAttribute(
+      "style",
+      "font-size: 24px; color: #214049; margin-top: 10px; margin-bottom: 10px; margin-right:10px; "
+    );
+    heart.setAttribute("class", "far fa-heart likeBtn");
+  }
 }
-
-
-
 
 //@return list of users
-async function  getPost(){
-    let response = await fetch(URL2);
-    let data = await response.json();
-    return data;
+async function getPost() {
+  let response = await fetch(URL2);
+  let data = await response.json();
+  console.log(data);
+  return data;
 }
 
-function addLikes(){
-    getPost().then(posts => {
-        let name = document.getElementById("userName")
-        for (let i = 0; i < posts.length; i++){
-            // look for the entry with a matching `users` value
-            if (posts[i].username === name){
-            console.log(posts[i].likes+1)
-            }
-        }
-    });
-}
-addLikes()
+// function addLikes() {
+//   getPost().then((posts) => {
+//     let name = document.getElementById("userName");
+//     for (let i = 0; i < posts.length; i++) {
+//       // look for the entry with a matching `users` value
+//       if (posts[i].username === name) {
+//         console.log(posts[i].likes + 1);
+//       }
+//     }
+//   });
+// }
