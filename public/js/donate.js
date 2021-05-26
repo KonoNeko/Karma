f6b8e83549f491fa6b20f64ccdb13a82995e7430
diyarravandi <dravandi@my.bcit.ca>
2021-05-26 14:07:30 -0700

    edits

--- public/js/donate.js
+++ public/js/donate.js
@@ -1,39 +1,40 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
     apiKey: "AIzaSyA5oEJUCOc3V4zgGI9-wwMWmd-P6opmnWI",
     authDomain: "karma-535f3.firebaseapp.com",
-    databaseURL: "https://karma-535f3-default-rtdb.firebaseio.com",
+    databaseURL: "https://karma-535f3-default-rtdb.firebaseio.com/",
     projectId: "karma-535f3",
     storageBucket: "karma-535f3.appspot.com",
     messagingSenderId: "1023587584355",
     appId: "1:1023587584355:web:89bb521723bf4afd58eb56",
-    measurementId: "G-VTZ4TEWFBW"
+    measurementId: "G-VTZ4TEWFBW",
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
-  
-    
   const db = firebase.firestore();
   
+  const BASE_URL = "https://marlonfajardo.ca/karma/v1";
+  
   let info = {};
+  let username;
   get_firebase_info();
   
-  function get_firebase_info() {
-    let info = {};
+  async function get_firebase_info() {
     firebase.auth().onAuthStateChanged(function (user) {
-      db.collection("users")
+      return db
+        .collection("users")
         .doc(user.uid)
         .get()
         .then(function (doc) {
           let user = doc.data();
-          info['fullName'] = user.fullName;
-          info['email'] = user.email;
-          info['username'] = user.username; 
-          view_notifications(info.username);
-          loadRecommendedConnections(info.username);
+          console.log(user);
+          info.fullName = user.fullName;
+          username = user.username;
+          info.email = user.email;
+          info.username = user.username;
+          view_social_feed(info.username);
           loadWhatsNew();
+          loadRecommendedConnections(info.username);
         })
         .catch((error) => {
           console.log(`Error getting data: ${error}`);
